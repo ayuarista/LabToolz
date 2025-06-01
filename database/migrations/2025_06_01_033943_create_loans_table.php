9b621e6df8c59773fab96312732e10b689eaf257
ayuarista <aristaadewi8@gmail.com>
2025-06-01 11:54:30 +0800

    Add Loan model and migration for loans table

---
+++ database/migrations/2025_06_01_033943_create_loans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->date('loan_date'); //tanggal melakukan peminjaman
+            $table->date('return_date'); //tanggal pengembalian yang murid rencanakan
+            $table->enum('status', ['pending', 'approved', 'returned', 'late'])->default('pending'); //kalo murid minjem barang, berarti dia harus nunggu guru approved dulu baru bisa minjem.
+            $table->date('actual_return_date'); //digunakan kalo murid mengembalikan lewat dari kesepakatan waktu 'return date'
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loans');
+    }
+};
