f369141b67e2011502acc31ac8d4cf51b2b10a05
ayuarista <aristaadewi8@gmail.com>
2025-06-01 12:23:48 +0800

    add loan item tables and model

---
+++ app/Models/LoanItem.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LoanItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'loan_id',
+        'item_id',
+        'quantity',
+        'note'
+    ];
+
+    public function loan()
+    {
+        return $this->belongsTo(Loan::class);
+    }
+
+    public function item()
+    {
+        return $this->belongsTo(Item::class);
+    }
+}
