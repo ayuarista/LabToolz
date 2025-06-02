3fa073afcaeab17308858b97032d5c05f2d2963c
ayuarista <aristaadewi8@gmail.com>
2025-06-02 09:25:24 +0800

    fixing controllers, and view template

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ItemController;
-use App\Http\Controllers\LoanController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('items', ItemController::class);
-Route::resource('loans', LoanController::class);
+
