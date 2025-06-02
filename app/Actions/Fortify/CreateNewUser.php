0e842d21efc4e6c3555630acac6e88e05f5eb6ec
mithayuliantari <kadekmitha21@gmail.com>
2025-06-02 21:36:02 +0800

    update validation teacher in CreateNewUser.php

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -3,6 +3,8 @@
 namespace App\Actions\Fortify;
 
 use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
@@ -11,21 +13,28 @@ class CreateNewUser implements CreatesNewUsers
 {
     public function create(array $input): User
     {
-        validator::make($input,[
-            'name'=>['required', 'string', 'max:255'],
-            'email'=>['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password'=>['required', 'string', 'min:8', 'confirmed'],
+        Validator::make($input, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'role' => ['required', 'in:teacher,student'],
         ])->validate();
 
+        // Validasi jika role teacher harus pakai email sekolah
+        if ($input['role'] === 'teacher' && !str_ends_with($input['email'], '@school.sch.id')) {
+            throw ValidationException::withMessages([
+                'email' => ['Only school email can register as teacher.'],
+            ]);
+        }
+
         $user = User::create([
-            'name'=>$input['name'],
-            'email'=>$input['email'],
-            'password'=>Hash::make($input['password']),
+            'name' => $input['name'],
+            'email' => $input['email'],
+            'password' => Hash::make($input['password']),
         ]);
 
-
-        // default role (student)
-        $user->assignRole('student');
+        // Berikan role sesuai input
+        $user->assignRole($input['role']);
 
         return $user;
     }
