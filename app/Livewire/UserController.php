1d6436c14ddc73b4ac289588b784a6c10e4b28c7
Erlin Ariani <27erlinariani@gmail.com>
2025-06-02 12:21:39 +0800

    add user controller

--- app/Livewire/UserController.php
+++ app/Livewire/UserController.php
@@ -3,9 +3,15 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\User;
 
 class UserController extends Component
 {
+    public $users;
+    public function mount()
+    {
+        $this->users = User::all();
+    }
     public function render()
     {
         return view('livewire.user-controller');
