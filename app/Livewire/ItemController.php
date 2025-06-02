5616ae8775415f578382abccbeeee28ea19699c6
Erlin Ariani <27erlinariani@gmail.com>
2025-06-02 12:25:20 +0800

    add item controller

---
+++ app/Livewire/ItemController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use App\Models\Item;
+
+class ItemController extends Component
+{
+    public $items;
+    public function mount()
+    {
+        $this->items = item::all();
+        
+    }
+    public function render()
+    {
+        return view('livewire.item-controller');
+    }
+}
