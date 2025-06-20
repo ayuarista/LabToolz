d404f16788f88eff3fe5d4ed5be3f94086e02d08
ayuarista <aristaadewi8@gmail.com>
2025-06-20 22:04:56 +0800

    refactor: remove logs property and initialization from HistoryController

--- app/Livewire/HistoryController.php
+++ app/Livewire/HistoryController.php
@@ -7,12 +7,6 @@ use App\Models\History;
 
 class HistoryController extends Component
 {
-    public $logs;
-
-    public function mount()
-    {
-        $this->logs = History::latest()->get();
-    }
     public function render()
     {
         return view('livewire.history-controller');
